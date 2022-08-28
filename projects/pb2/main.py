d87c7f73f311d1aae90be11b8095c2ca05433cd2
work <math.dumont.md@gmail.com>
2022-08-28 22:41:32 +0200

    pb2 upgrade

--- projects/pb2/main.py
+++ projects/pb2/main.py
@@ -10,7 +10,10 @@ By considering the terms in the Fibonacci sequence whose values do not exceed fo
 
 """
 
-def fibo(n):
+import functools
+
+
+def fibonacci(n):
     prec = 0
     fibo = 1
 
@@ -18,16 +21,49 @@ def fibo(n):
         tmp = fibo
         fibo = fibo + prec
         prec = tmp
-        
+
         yield fibo
 
 
-if __name__ == "__main__":
-    sum = 0
-    for i in fibo(4e6):
-        print(i)
-        if i % 2 == 0:
-            
-            sum += i
+def fn():
 
-    print(f"By considering the terms in the Fibonacci sequence whose values do not exceed four million, the sum of the even-valued terms is {sum}")
+    sum = 0
+    for i in fibonacci(4e6):
+        if i % 2 == 0:
+
+            sum += i
+    return sum
+
+
+def even_fibonacci(n):
+    """
+    2 3 5 8 13 21 34 55 89 144 
+    +     +       +         +
+    every 3 is even in the fibonacci sequence
+    and even there is a property,
+
+    Even(n) = 4 * even(n-1) + even(n-1) 
+    even(2) = 4 * even(1) + even(0) = 4*2 + 0 = 8
+    even(3) = 4 * even(2) + even(1) = 4*8 + 2 = 32
+    even(4) = 4 * even(3) + even(2) = 4*34 + 8 = 136 + 8 = 144
+
+    """
+    n_minus_two = 0
+    n_minus_one = 2
+
+    while n_minus_one < int(n):
+        yield n_minus_one
+        tmp = n_minus_one
+        n_minus_one = 4*n_minus_one + n_minus_two
+        n_minus_two = tmp
+
+
+def fn2():
+    return functools.reduce(lambda a, x: a+x, even_fibonacci(4e6), 0)
+
+
+if __name__ == "__main__":
+    sum = fn2()
+
+    print(
+        f"By considering the terms in the Fibonacci sequence whose values do not exceed four million, the sum of the even-valued terms is {sum}")
