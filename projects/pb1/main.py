e721bd857b743990d017e223803a6d31883905b4
work <math.dumont.md@gmail.com>
2022-08-28 19:55:37 +0200

    improvements pb1

--- projects/pb1/main.py
+++ projects/pb1/main.py
@@ -5,16 +5,27 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 
 """
+from functools import reduce
+from math import floor
 N = 1000
 
 
 def is_multiple_of(left, right):
     return right % left == 0 or left % right == 0
 
-if __name__ == "__main__":
-    summed = 0
-    for i in range(2, N):
-        if is_multiple_of(i, 3) or is_multiple_of(i, 5):
-            summed += i
+# def sumOfMultipleUnder(mult, limit):
+#     return reduce(lambda x, y: y + x, range(1, floor(limit/mult) + 1), 0) * mult
 
-    print(f"The sum of all the multiples of 3 or 5 below 1000 is {summed}")
+
+def sumOfMultipleUnder(mult, n):
+    p = n // mult
+    x = mult * p * (p+1)  // 2
+    return x 
+
+
+if __name__ == "__main__":
+    n = 999
+
+    sum = sumOfMultipleUnder(3, n) + sumOfMultipleUnder(5, n)  - sumOfMultipleUnder(15, n)
+
+    print(f"The sum of all the multiples of 3 or 5 below 1000 is {sum}")
