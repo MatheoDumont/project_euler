076e2cec59b0127ed9c54c61928491860297be06
root <root@BA00058.Bioaster.local>
2023-03-23 17:42:30 +0100

    pb021 sum

--- projects/pb021/main.cpp
+++ projects/pb021/main.cpp
@@ -3,35 +3,63 @@
 
 using namespace std;
 
+/*
+* Sum of all proper divisor of n (starting from 1, without n).
+* if n is one, should return 0
+*/
 int sum_proper_divisor(int n)
 {
+    
+    float n_f = (float)n;
     int somme = 1;
-    int div = 2;
-    float upper_bound;
-    float fractional;
-
+    float div = 2;
+    float whole, fractional;
     float s = sqrt(n);
-    while (div < s)
+
+
+    while (div <= s)
     {
-        upper_bound = modf(n / div, &fractional);
-        if (fractional==0.0)
-            somme += div + upper_bound;
-        
-        div += 1;
+        fractional = modf(n_f / div, &whole);
+
+        if (fractional == 0.0) {
+            if (div == whole) 
+                somme += div;
+            else
+                somme += div + whole;
+        }
+
+        div++;
     }
     return somme;
 }
 
-int main()
-{
+int test_sum_proper_divisor()
+{   
+    cout << "sum_proper_divisor(220)" << endl;
     if (int s = sum_proper_divisor(220); s != 284)
     {
         cout << "sum_proper_divisor of 220 should be 284, " << s << " given !" << endl;
         exit(1);
     }
+    cout << "sum_proper_divisor(284)" << endl;
+
+    if (int s = sum_proper_divisor(284); s != 220)
+    {
+        cout << "sum_proper_divisor of 284 should be 220, " << s << " given !" << endl;
+        exit(1);
+    }
+    cout << "sum_proper_divisor(5)" << endl;
     if (int s = sum_proper_divisor(5); s != 1)
     {
         cout << "sum_proper_divisor of 5 should be 1, " << s << " given !" << endl;
         exit(1);
+    }
+}
+
+int main()
+{
+    test_sum_proper_divisor();
+    for (int i = 2; i < 100; i++) {
+        cout << i << " == " << sum_proper_divisor(i) << endl;
     }
 }
